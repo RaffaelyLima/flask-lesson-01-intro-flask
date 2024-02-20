dd648f96dc0ca9536337b9bc05e58b5cd7503569
albalopes <alba.lopes@ifrn.edu.br>
2024-02-20 00:27:39 +0000

    Revert "Adição de novas rotas"
    This reverts commit 6382b2fb95ee7045f3b663c5d5e395d8f4bb0f8f.

--- app.py
+++ app.py
@@ -3,13 +3,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Bem vindos a disciplina de Desenvolvimento Web'
-
-@app.route('/email')
-@app.route('/contato')
-def contato():
-    return 'alba.lopes@ifrn.edu.br'
-
-@app.route('/endereco')
-def endereco():
-    return 'Rua Brusque, 2629'
+    return 'Olá Mundo'
+    
