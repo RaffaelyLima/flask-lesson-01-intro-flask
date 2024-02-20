6382b2fb95ee7045f3b663c5d5e395d8f4bb0f8f
albalopes <alba.lopes@ifrn.edu.br>
2024-02-20 00:03:53 +0000

    Adição de novas rotas

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá Mundo'
-    
+    return 'Bem vindos a disciplina de Desenvolvimento Web'
+
+@app.route('/email')
+@app.route('/contato')
+def contato():
+    return 'alba.lopes@ifrn.edu.br'
+
+@app.route('/endereco')
+def endereco():
+    return 'Rua Brusque, 2629'
